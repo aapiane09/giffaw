fbf836592548f65b8fefef52ce3faf47098b531f
aapiane09 <aapiane09@gmail.com>
2016-12-07 16:11:33 -0800

    Finished basic goals

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,50 @@
 $(document).on("ready", function(){
+  // function onReady(event)
+ $.ajax({
+   method: "GET",
+   url: "http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC",
+   dataType: "json",
+   success: logSuccess,
+   error: logError,
+   complete: logComplete
+ });
 
+//search for images
+   $('form').on("submit", searchReq);
+    function searchReq(event){
+      event.preventDefault();
+      //clear out gif-gallery
+      $('.gif-gallery').each(function(){
+        $(this).text("");
+      })
+      $.ajax({
+        method: "GET",
+        url: "http://api.giphy.com/v1/gifs/search",
+        dataType: "json",
+        data: $('form').serialize(),
+        success: logSuccess,
+        error: logError,
+        complete: logComplete
+      });
+    };
+    function logSuccess(responseData){
+     console.log(responseData);
+     var imgArray = responseData.data;
+     imgArray.forEach(function (object){
+       var gifImg = object.images.fixed_height.url;
+       $('.gif-gallery').append('<img src="' + gifImg + '">');
+     });
+    }
+    function logError(){
+     console.log("Error!");
+    }
+    function logComplete(responseData){
+     console.log("Complete!");
+    }
+    //append new input for load more
+    //ajax to get serialize plus concat offset var, var being increased by 25 each time
 });
+
+//base API URL: http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC
+//trending gifs: http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC
+//search query path: /v1/gifs/search
